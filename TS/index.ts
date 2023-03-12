5ec5ac481f3af22e19a0a20d34f4b0667a798a5e
Mavlon_Yusuf <113101527+Mavlonyusuf@users.noreply.github.com>
2023-03-12 11:59:45 +0500

    Learned Utility-types

--- TS/index.ts
+++ TS/index.ts
@@ -226,70 +226,399 @@
 // console.log(len([0]));  // OK
 // console.log();
 
-class Users {
-    name : string
-    age : number
+// class Users {
+//     name : string
+//     age : number
 
-    // constructor
+//     // constructor
 
-    constructor(name : string, age : number) {
-        this.name = name
-        this.age = age
-    }
+//     constructor(name : string, age : number) {
+//         this.name = name
+//         this.age = age
+//     }
 
-    // method
+//     // method
 
-    info(value : number) {
-        return `${this.name} ${value} yildan so'ng ${this.age + value} yoshga to'ladi`
-    }
-    isYoung ():string {
-        if(this.age < 18) {
-            return `${this.name} hali balo'gat yoshiga yetmagan`
-        }else if (this.age === 18) {
-            return `${this.name} hozirda balo'gat yoshida`
-        }
-         else {
-            return `${this.name} balog'at yoshiga yetganiga ${this.age - 18} yil bo'lgan`
-        }
-    }
+//     info(value : number) {
+//         return `${this.name} ${value} yildan so'ng ${this.age + value} yoshga to'ladi`
+//     }
+//     isYoung ():string {
+//         if(this.age < 18) {
+//             return `${this.name} hali balo'gat yoshiga yetmagan`
+//         }else if (this.age === 18) {
+//             return `${this.name} hozirda balo'gat yoshida`
+//         }
+//          else {
+//             return `${this.name} balog'at yoshiga yetganiga ${this.age - 18} yil bo'lgan`
+//         }
+//     }
 
-    // 
-    static description ():string  {
-        return `Ushbu klass foydalanuvchi haqida ma'lumot beradi...`
-    }
+//     // 
+//     static description ():string  {
+//         return `Ushbu klass foydalanuvchi haqida ma'lumot beradi...`
+//     }
 
-    // Getter && Setter
+//     // Getter && Setter
 
-    get changeName () :string  {
-        return this.name
-    }
-    set changeName (value : string) {
-        this.name = value
-    }
+//     get changeName () :string  {
+//         return this.name
+//     }
+//     set changeName (value : string) {
+//         this.name = value
+//     }
 
-    // changeAge
-    get changeAge () : number {
-        return this.age
-    }
-    set changeAge(value : number) {
-        this.age = value
-    }
-}
-const Mavlon : Users = new Users("Mavlon", 22)
-console.log(Mavlon);
-console.log(Mavlon.info(5));
-console.log(Mavlon.isYoung());
+//     // changeAge
+//     get changeAge () : number {
+//         return this.age
+//     }
+//     set changeAge(value : number) {
+//         this.age = value
+//     }
+// }
+// const Mavlon : Users = new Users("Mavlon", 22)
+// console.log(Mavlon);
+// console.log(Mavlon.info(5));
+// console.log(Mavlon.isYoung());
 
-const Asliddin : Users = new Users("Asliddin", 18)
-console.log(Asliddin);
-console.log(Asliddin.info(5));
-console.log(Asliddin.isYoung());
-console.log(Users.description());
+// const Asliddin : Users = new Users("Asliddin", 18)
+// console.log(Asliddin);
+// console.log(Asliddin.info(5));
+// console.log(Asliddin.isYoung());
+// console.log(Users.description());
 
-// Bitta classda faqatgina bitta constructor ishlatish mumkin. Lekin cheksiz method ham staticlar ishlatsa bo'ladi
-// Static --- bu classga tegishli bo'lgan ma'lumotlardir. 
+// // Bitta classda faqatgina bitta constructor ishlatish mumkin. Lekin cheksiz method ham staticlar ishlatsa bo'ladi
+// // Static --- bu classga tegishli bo'lgan ma'lumotlardir. 
 
-Mavlon.name = "Asadbek"
-console.log(Mavlon.name); // Asadbek
-Asliddin.age = 25
-console.log(Asliddin.age); // 25
+// Mavlon.name = "Asadbek"
+// console.log(Mavlon.name); // Asadbek
+// Asliddin.age = 25
+// console.log(Asliddin.age); // 25
+
+// // instanceof ====== obyektni qaysi klassdan olinganligini tekshiradi. For instance : 
+// console.log(Mavlon instanceof Users);  //true mavjud bolsa true aks xolda false qaytaradi
+
+// Extends ============ me'ros olish
+
+// class Person {
+//     _name : string
+//     _age : number
+
+//     constructor(name : string, age : number) {
+//         this._name = name
+//         this._age = age
+//     }
+
+//     greeting():string {
+//         return `Hello my name is ${this._name}. I'm ${this._age} years old.`
+//     }
+// }
+
+// const student : Person = new Person("Mavlon", 22)
+
+// class Students extends Person {
+//     _job : string
+//     _skills : string[]
+//     greeting(): string {
+//         // extends method's
+//         const parentGreeting = super.greeting()
+//         return `${parentGreeting} I'm a ${this._job}. I know ${this._skills[0]}, ${this._skills[1]}, ${this._skills[2]} and etc`
+//     }
+
+//     constructor(name : string, age : number, job : string, skills : string[]) {
+//         super(name, age)
+//         this._job = job
+//         this._skills = skills
+//     }
+// }
+// const Mavlon : Students = new Students("Mavlon", 22, "Front-End Developer", ["JavaScript", "VueJs", "VueX", "TypeScript"])
+// console.log(Mavlon.greeting());
+
+// ================ INTERFACES interface larni typelardan asosiysi farqi => iterface larni bitta nima bilan bir nechtasini e'lon qilsa 
+// bo'ladi. Typelarda esa aksincha. Typelarni birlashtirsa boladi. For instance : 
+// bir nechta interface larni bir xil nom bilan atash
+// interface Human {
+//     _name : string
+//     _age : number
+// }
+// interface Human {
+//     seyHello() :string;          //seyHello: () => string
+// }
+
+// bir nechta typelarni birlashtirish
+// type Users1 = {
+//     _name: string
+//     _age : number
+// }
+// type Users2 = {
+//     _greeting() : string
+// }
+
+// type Users = Users1 & Users2 & {
+//     _job : string | number
+// }
+
+// ===================== interface larni classlardan farqlari for intance : 
+// classlarda bitta class faqatgina bitta classdan vorislik ola oladi. Interface larda esa cheksiz for instance :
+
+// class User {
+//     _name : string
+//     _age : number
+
+//     constructor(name : string, age : number) {
+//         this._name = name
+//         this._age = age
+//     }
+// }
+// class User1 {
+//     _salary : number
+
+//     constructor(salary : number) {
+//         this._salary = salary
+//     }
+// }
+
+// class Person extends User /*, User1*/ {     //Error beradi
+//     _job : string
+//     _experience : boolean
+
+//     constructor(name : string, age : number, job : string, experience : boolean) {
+//         super(name, age)
+//         this._job = job
+//         this._experience = experience
+//     }
+// }
+// interface UserA {
+//     _name : string
+// }
+// interface UserB {
+//     _age : number
+// }
+// interface UserC {
+//     _job : string
+// }
+// interface UserGeneral extends UserA, UserB, UserC {
+//     seyHello () :string ; 
+// }
+
+// class IUser implements UserA {
+//     _name : string
+//     constructor (name : string) {
+//         this._name = name
+//     }
+// }
+
+// //  = ========================== Abstract Classes  => Faqatgina vorislik olish uchun qo'llaniladigan classlar, bu classlardan new OBJ yasab bo'lmaydi
+// // abstract classlarga abstract methodlar berish ham mumkin. Shunda bu methodlar majburiy bo'lib qoladi
+
+// abstract class Users {
+//     _name : string
+//     _age : number
+
+//     constructor (name : string, age : number) {
+//         this._name = name
+//         this._age = age
+//     }
+//     abstract greeting(): string;
+// }
+// // const newUser = new Users("Mavlon", 22)  Error beradi
+
+// class UserA extends Users {
+//     _job : string
+
+//     constructor(name: string, age: number, job: string) {
+//         super(name, age) 
+//         this._job = job
+//     }
+//     greeting(): string {
+//         return `Mening ismim ${this._name}. Yoshim ${this._age} da. ${this._job} bo'lib ishlayman`
+//     }
+// }
+// const newUser = new UserA("Mavlon", 22, "Front-End developer")
+// console.log(newUser.greeting());
+
+// class MySafe {
+//     private secretKey = 12345;
+//   }
+//   // In a JavaScript file...
+//   const s = new MySafe();
+//   // Will print 12345
+//    console.log(s.secretKey);  // this is true console.log(s["secretKey"]);
+
+// class Dog {
+//     #barkAmount = 0;
+//     personality = "happy";
+   
+//     constructor() {}
+//   }
+
+//   const newDog = new Dog()
+//   console.log(newDog.personality);
+
+// namespace Utils {
+//     export function log (text : string, color : string, fontSize? : string):void {
+//         if(fontSize) {
+//             console.log(`%c${text}`, `color : ${color}; font-size : ${fontSize}`);
+            
+//         }else {
+//             console.log(`%c${text}`, `color : ${color}`)
+//         }
+//     }
+// }
+
+// namespace Animals {
+//     export abstract class Animal {
+//         name : string
+        
+//         constructor(name : string) {
+//             this.name = name
+//         }
+//         abstract say() :void;
+//     }
+//     export class Dogs extends Animals.Animal {
+//         private sound : string
+//         constructor(name : string, sound : string) {
+//             super(name)
+//             this.sound = sound
+            
+//         }
+//         say() : void {
+//             Utils.log(`${this.name} ${this.sound} degan ovoz chiqaradi`, 'red', '25px');
+            
+//         }
+//     }
+
+// }
+// const cat = new Animals.Dogs("Arslon", "Woof")
+// cat.say()
+// function sound <Type>(x:Type) : Type {
+//     return x
+// }
+// let result = sound(12)
+// console.log(result);
+// const sound = <Type> (x:Type) => x
+// const result = sound(15)
+// console.log(result);
+
+// type A<T> = T
+// const echo : <T>(x:T) => T = <T>(x:T) : T => {
+//     return x
+// }
+// const echo = <T> (x:T) => x 
+// const echo1 = echo([54,87,2158,5])
+// console.log(echo1);
+
+// function identity<Type>(arg: Type): Type {
+//     return arg;
+//   }
+   
+//   let myIdentity: <Type>(arg: Type) => Type = identity;
+//   console.log(myIdentity(45));
+  // function identity<Type>(arg: Type): Type {
+  //   return arg;
+  // }
+   
+  // let myIdentity: <Input>(arg: Input) => Input = identity
+  // console.log(myIdentity("sjbsjdns"));
+
+  // class getData <Type> {
+  //   name : Type
+  //   password : Type
+
+  //   addUser : (name : Type, password : Type) => Type
+  // }
+
+  // let newUser = new getData <string>()
+  // newUser.name = "Mavlon"
+  // newUser.password = "123456user"
+  // newUser.addUser = function (x, y) {
+  //   return x + y
+  // }
+
+//   function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+//     return obj[key];
+//   }
+   
+//   let x = { a: 1, b: 2, c: 3, d: 4 };
+   
+//  console.log( getProperty(x, "a"));
+ 
+
+// type Point = { x: number; y: number };
+// type P = keyof Point;
+// const Num : P = "x" 
+// console.log(Num);
+// let Arr1 = [1, true, "bir"]
+// type A<T> = T extends (infer U) [] ? U : never
+// type B = typeof Arr1
+// type C = A<B>
+
+// ======================== Partial =========================
+
+// type MyPartial<T> = {[P in keyof T]? : T[P]}
+
+// type Obj = {
+//   name: string
+//   age : number
+// }
+// type Obj1 = MyPartial <Obj>
+
+// type Obj1 = {
+//   name?: string | undefined;
+//   age?: number | undefined;
+// }
+
+// type Obj2 = Partial <Obj>
+
+// type Obj2 = {
+//   name?: string | undefined;
+//   age?: number | undefined;
+// }
+
+// ===================== Required ==========================
+
+// type User = {
+//   name? : string
+//   age? :number
+// }
+
+// type User1 = Required <User>
+
+// // type User1 = {
+// //   name: string;
+// //   age: number;
+// // }
+
+// type MyRequired <T> = {[P in keyof T]-? : T[P]}
+// type User2 = MyRequired <User>
+
+
+// // type User2 = {
+// //   name: string;
+// //   age: number;
+// // }
+
+// ===================== Record ==========================
+
+// interface CatInfo {
+//   age: number;
+//   breed: string;
+// }
+ 
+// type CatName = "miffy" | "boris" | "mordred";
+ 
+// const cats: Record<CatName, CatInfo> = {
+//   miffy: { age: 10, breed: "Persian" },
+//   boris: { age: 5, breed: "Maine Coon" },
+//   mordred: { age: 16, breed: "British Shorthair" },
+//   // mavlon : {age : 22, breed : "lorem ipsum"}  Error beradi. Faqatgina korsatilgan keylardan foydalanish mumkin
+// };
+
+// ===================== NonNullable ========================== 
+
+// Faqatgina qiymat mavjud bolgan turdagi datalarni qaytaradi
+type Result = string | boolean | null | undefined
+type result1 = NonNullable<Result>  //type result1 = string | boolean
+
+type MyNonNullable <T> = T extends null | undefined ? never : T
+type result2 = MyNonNullable<Result>  //type result2 = string | boolean
+ 
